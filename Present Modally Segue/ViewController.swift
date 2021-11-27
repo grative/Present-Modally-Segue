aa68131276cd36c1fac695fb7bbd84defbb6e916
Eren <eren>
2021-11-27 16:24:21 +0300

    Initial Commit

---
+++ Present Modally Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Present Modally Segue
+//
+//  Created by Eren Yürekli on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
